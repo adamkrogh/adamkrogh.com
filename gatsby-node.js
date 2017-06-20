62d78b6bc40ab23488af55bda7078321c788ae73
Adam Krogh <adamkrogh@gmail.com>
2017-06-19 19:39:32 -0600

    Removed a bunch of unused dependencies.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,8 @@
-var rucksack = require('rucksack-css');
-var lost = require('lost');
 var cssnext = require('postcss-cssnext');
 
-exports.modifyWebpackConfig = function(config, env) {
+exports.modifyWebpackConfig = function (config, env) {
     config.merge({
         postcss: [
-            lost(),
-            rucksack(),
             cssnext({
                 browsers: ['>1%', 'last 2 versions']
             })
--- html.js
+++ html.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import DocumentTitle from 'react-document-title';
 import { prefixLink } from 'gatsby-helpers';
 import styleSheet from 'styled-components/lib/models/StyleSheet';
 
 const BUILD_TIME = new Date().getTime();
 
-module.exports = React.createClass({
-    displayName: 'HTML',
-    propTypes: {
-        body: React.PropTypes.string
-    },
+class Html extends React.Component {
+    static displayName = 'HTML';
+    static propTypes: {
+        body: PropTypes.string
+    };
+
     render() {
         const { body, route } = this.props;
         const title = DocumentTitle.rewind();
@@ -57,12 +59,11 @@ module.exports = React.createClass({
                         id="react-mount"
                         dangerouslySetInnerHTML={{ __html: this.props.body }}
                     />
-                    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" />
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" />
-                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" />
                     <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
                 </body>
             </html>
         );
     }
-});
+}
+
+export default Html;
